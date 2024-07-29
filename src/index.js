08914a1e48be9454bafccb6d629c6867c3f84ea2
molcham <sonchaemin89@sookmyung.ac.kr>
2024-07-29 21:32:29 +0900

    JSX에서 자바스크립트 사용하기 실습

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import ReactDOM from 'react-dom';
 
+const WINS={
+    rock:'scissor',
+    scissor:'paper',
+    paper:'rock',
+}
+
+function getResult(left,right){
+
+  if (WINS[left] === right) return '승리';
+  else if (left === WINS[right]) return '패배';
+  return '무승부';
+
+}
+
+function handleClick(){
+    alert('가위바위보!');
+}
+
+const me='rock';
+const other='scissor';
+
+
+
 ReactDOM.render(
     <>
      <h1 id="title">가위바위보</h1>
-     <button class="hand">가위</button>
-     <button class="hand">바위</button>
-     <button class="hand">보</button>
+     <br></br>
+     <h2>{getResult(me,other)}</h2>
+     <br></br>
+     <button onClick={handleClick}>가위</button>
+     <button onClick={handleClick}>바위</button>
+     <button onClick={handleClick}>보</button>
     </>
    , document.getElementById('root')
 );
