0940246a3ad5dd5bc4bb47675d330c4107a23e12
molcham <sonchaemin89@sookmyung.ac.kr>
2024-07-29 21:01:00 +0900

    간단한 가위바위보 게임 실습

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>안녕 리액트!</h1>);
+ReactDOM.render(
+    <>
+     <h1 id="title">가위바위보</h1>
+     <button class="hand">가위</button>
+     <button class="hand">바위</button>
+     <button class="hand">보</button>
+    </>
+   , document.getElementById('root')
+);
