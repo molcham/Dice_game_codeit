aff81273d9fbb5ae69f5d56a61da7986b40ebb18
molcham <sonchaemin89@sookmyung.ac.kr>
2024-08-09 18:22:31 +0900

    props실습 해설_ 구조분해 사용

--- src/HandIcon.js
+++ src/HandIcon.js
@@ -2,17 +2,16 @@ import rockImg from './assets/rock.svg';
 import scissorImg from './assets/scissor.svg';
 import paperImg from './assets/paper.svg';
 
-let src;
-function HandIcon({value}) {
-  if(value ==='rock'){
-    src=rockImg;
-  }
-  else if(value ==='scissor'){
-    src=scissorImg;
-  }
-  else {
-    src=paperImg;
-  }
+const IMAGES = {
+    //IMAGES라는 객체를 생성하기
+  rock: rockImg,
+  scissor: scissorImg,
+  paper: paperImg,
+};
+
+function HandIcon({ value }) {
+    //구조분해 사용하기
+  const src = IMAGES[value];
   return <img src={src} alt={value} />;
 }
 
